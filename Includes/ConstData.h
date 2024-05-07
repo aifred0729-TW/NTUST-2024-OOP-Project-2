0e801e1867b30f39f6ffae83fbdb5608a306764f
aifred0729-TW <aifred0729tw@gmail.com>
2024-05-07 22:46:33 +0800

    REFACTOR : Add Attribute Struct in ConstData.h

--- Includes/ConstData.h
+++ Includes/ConstData.h
@@ -1,3 +1,5 @@
+#include "HeaderPack.h"
+
 #define BANNER = \
 R"(
       ___           ___           ___                    ___           ___           ___                    ___                       ___           ___                        
@@ -24,6 +26,16 @@ R"(
      \/__/                       \/__/         \/__/                                              \/__/         \|__|         \/__/                     \/__/         \/__/    
 )"
 
+struct Attribute {
+	uint8_t focus;     // Focus
+	uint8_t maxHP;     // Max HP
+	uint8_t SPD;       // Speed
+	uint8_t ACC;       // Hit Rate
+	uint8_t PA;        // Physical Attack
+	uint8_t MA;        // Magic Attack
+	uint8_t PD;        // Physical Defense
+	uint8_t MD;        // Magic Defense
+};
 
 enum SKILL_TYPE {
 	ATTACK,         // Attack
