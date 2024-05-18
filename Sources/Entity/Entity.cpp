ef3ee11635e30db341a4e17da60130b731fd1c2a
天酒喵 <howardmeow777@gmail.com>
2024-05-18 23:44:45 +0800

    FEAT: Implement Getters and Setters.

--- Sources/Entity/Entity.cpp
+++ Sources/Entity/Entity.cpp
@@ -5,3 +5,66 @@
 bool Entity::isInRange(std::vector<Entity*>) {
     return 0;
 }
+
+void Entity::SetAttribute(const Attribute& attribute) {
+    this->attribute.maxHP = attribute.maxHP;
+    this->attribute.maxFocus = attribute.maxFocus;
+    this->attribute.HP = attribute.HP;
+    this->attribute.focus = attribute.focus;
+    this->attribute.SPD = attribute.SPD;
+    this->attribute.ACC = attribute.ACC;
+    this->attribute.PA = attribute.PA;
+    this->attribute.MA = attribute.MA;
+    this->attribute.PD = attribute.PD;
+    this->attribute.MD = attribute.MD;
+}
+void Entity::SetSkill(const Skill& skill) {
+    this->skill.skillID = skill.skillID;
+    this->skill.coolDownTime = skill.coolDownTime;
+    this->skill.dice = skill.dice;
+    this->skill.skillUser = skill.skillUser;
+    this->skill.skillTargets = skill.skillTargets;
+}
+void Entity::SetHP(const uint32_t HP) {
+    this->HP = HP;
+}
+void Entity::SetStatus(const uint8_t status) {
+    this->status = status;
+}
+void Entity::SetEventID(const uint8_t eventID) {
+    this->eventID = eventID;
+}
+void Entity::SetWeaponID(const uint8_t weaponID) {
+    this->weaponID = weaponID;
+}
+void Entity::SetArmorID(const uint8_t armorID) {
+    this->armorID = armorID;
+}
+void Entity::SetAccessoryID(const uint8_t accessoryID) {
+    this->accessoryID = accessoryID;
+}
+
+Attribute Entity::GetAttribute(void) {
+    return attribute;
+}
+Skill     Entity::GetSkill(void) {
+    return skill;
+}
+uint32_t  Entity::GetHP(void) {
+    return HP;
+}
+uint8_t   Entity::GetStatus(void) {
+    return status;
+}
+uint8_t   Entity::GetEventID(void) {
+    return eventID;
+}
+uint8_t   Entity::GetWeaponID(void) {
+    return weaponID;
+}
+uint8_t   Entity::GetArmorID(void) {
+    return armorID;
+}
+uint8_t   Entity::GetAccessoryID(void) {
+    return accessoryID;
+}
