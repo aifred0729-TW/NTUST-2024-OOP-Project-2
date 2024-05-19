22efb8019bb3300e97808fada9201b207cb6cf96
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2024-05-19 15:17:58 +0800

    FEAT add Action class

--- Includes/Incident/Field.h
+++ Includes/Incident/Field.h
@@ -16,37 +16,42 @@
 #include <Role.h>
 #include <Dice.h>
 
+struct StatusCount {
+	uint8_t poisoned;
+	uint8_t bleed;
+	uint8_t dizziness;
+	uint8_t angry;
+};
+
 class Action {
 private:
-	Dice dice;
+	Dice* dice;
 	Entity* obj;
-	uint8_t priority;
+	StatusCount* count;
+	uint32_t priority;
 	uint8_t turn;
 	uint8_t entityID;
-	uint8_t statusTurn;
 
 public:
 	Action(Entity* val, uint8_t mode, uint8_t ID);
 
 	void SetObj(Entity* val) { obj = val; }
-	void SetPriority(uint8_t val) { priority = val; }
+	void SetPriority(uint32_t val) { priority = val; }
 	void SetTurn(uint8_t val) { turn = val; }
 	void SetEntityID(uint8_t val) { entityID = val; }
-	void SetStatusTurn(uint8_t val) { statusTurn = val; }
 
-	Dice    GetDice(void) { return dice; }
+	Dice* GetDice(void) { return dice; }
 	Entity* GetObj(void) { return obj; }
-	uint8_t GetPriority(void) { return priority; }
-	uint8_t GetTurn(void) { return turn; }
-	uint8_t GetEntityID(void) { return entityID; }
-	uint8_t GetStatusTurn(void) { return statusTurn; }
+	StatusCount* GetCount(void) { return count; }
+	uint32_t GetPriority(void) { return priority; }
+	uint8_t	GetTurn(void) { return turn; }
+	uint8_t	GetEntityID(void) { return entityID; }
 };
 
 class Field {
 private:
 	Action* currEvent;                      // Current Event
-	std::vector<Action*> enemyEngage;       // Engage Enemys
-	std::vector<Action*> roleEngage;        // Engage Roles
+	std::vector<Action*> eventQueue;        // Engage Entity
 
 public:
 	void MainPhase(Action*);
@@ -54,21 +59,18 @@ public:
 	void DamagePhase(Action*);
 	void ExitPhase(void);
 
-	void Init(void);
 	void ChooseSkill(Role*);
 	void ChooseTarget(Role*);
 	void UsingFocus(Role*);
 	// Detect Status and Compute
 
 	Action* RefreshEvent(void);
-	bool AllRoleDead(void);
-	bool AllEnemyDead(void);
 
 public:
-	// Set Combat Configuation
+	// Set Combat Configuation; Put the players into first parameter and the enemies into second parameter.
 	Field(std::vector<Entity*>, std::vector<Entity*>);
-	// Set Combat Configuation
 	~Field(void);
+
 	// Start Combat
 	void StartBattle(void);
 };
