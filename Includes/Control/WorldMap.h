795115dfd5caa9421ebbcda49853f443fd80db02
aifred0729-TW <aifred0729tw@gmail.com>
2024-06-08 16:41:30 +0800

    FEAT : New function loadMap and SetMap is ready to use

--- Includes/Control/WorldMap.h
+++ Includes/Control/WorldMap.h
@@ -9,20 +9,38 @@
 #include <string>
 #include <vector>
 #include <windows.h>
+#include <fstream>
+#include <sstream>
+#include "ConstData.h"
 
 class WorldMap {
 private:
-    static std::vector<std::vector<char>> board; // Map Storge
-    static std::vector<std::vector<bool>> fog;   // War Fog (Make some lamp?)
+    // Map Structure
+    // 0 = Player
+    // 1 = Ground (Allow to Pass)
+    // 2 = Wall   (Not Allow to Pass)
+    // 3 = Tree   (Not Allow to Pass)
+    // 4 = Water  (Not Allow to Pass)
+    // 5 = Shop   (Not Allow to Pass)
+    // 6 = Enemy  (Not Allow to Pass)
+
+    std::vector<std::vector<int>> map; // Map Storge
+    std::vector<std::vector<bool>> fog;  // War Fog (Make some lamp?)
+
+    void loadMap(std::string mapFile);
 
 public:
-    WorldMap(/**/);
+    WorldMap() { ; };
 
-    std::vector<std::vector<char>>& GetBoard(void);
-    void SetBoard(const std::vector<std::vector<char>>&);
+    WorldMap(std::string mapFile) {
+        loadMap("../Resources/" + mapFile);
+    }
 
-    std::vector<std::vector<bool>>& GetFog(void);
-    void GetFog(const std::vector<std::vector<bool>>&);
+    std::vector<std::vector<int>> GetMap(void);
+    void SetMap(int row, int col, MAP_ELEMENT element);
+
+    //std::vector<std::vector<bool>>& GetFog(void);
+    //void GetFog(const std::vector<std::vector<bool>>&);
 
 public:
     // ...
--- Sources/Control/WorldMap.cpp
+++ Sources/Control/WorldMap.cpp
@@ -1,1 +1,33 @@
 #include "../../Includes/Control/WorldMap.h"
+#include "ConstData.h"
+
+void WorldMap::loadMap(std::string mapFile) {
+	using namespace std;
+
+	ifstream fp(mapFile);
+	stringstream ss("");
+	string stmp = "";
+	int itmp = 0;
+	int row = 0;
+
+	map.resize(50);
+
+	while (fp >> stmp) {
+		for (unsigned int i = 0; i < 140; i++) {
+			ss << stmp[i];
+			ss >> itmp;
+			ss.clear();
+			map[row].push_back(itmp);
+		}
+		row++;
+	}
+
+	return;
+}
+
+void WorldMap::SetMap(int row, int col, MAP_ELEMENT element) {
+	map[row][col] = element;
+	return;
+}
+
+std::vector<std::vector<int>> WorldMap::GetMap() { return map; }
