b87de901fbe8c48ff5f33519d1269be3edaed872
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2024-06-09 00:32:00 +0800

    FEATER: Build battle system by skill and UI.

--- Sources/Incident/Field.cpp
+++ Sources/Incident/Field.cpp
@@ -1,12 +1,15 @@
-﻿#include "../../Includes/Incident/Field.h"
-
+﻿#include <Field.h>
+#include <UI.h>
 // Public
 
 void Field::StartBattle(void) {
 	using namespace std;
 
+	UI::PreBattle(enemys, roles);
+
 	while (1) {
 		currEvent = RefreshEvent();
+
 		uint8_t currStatus = 0;// currEvent->GetObj()->GetStatus();
 		if (currStatus & DEAD) {
 			continue;
@@ -18,119 +21,25 @@ void Field::StartBattle(void) {
 		StatusCount* currentCount = currEvent->GetCount();
 		if (currentCount->dizziness) {
 			currentCount->dizziness--;
-			DamagePhase(currEvent);
 			continue;
 		}
 
 		MainPhase(currEvent);
-		BattlePhase(currEvent);
-		DamagePhase(currEvent);
+		RemoveDeadEntity();
 
-		try { ExitPhase(); }
+		try {
+			ExitPhase();
+		}
 		catch (const exception& e) {
-			cout << e.what();
+			UI::displayString(e.what(), 6, 9);
 			return;
 		}
 	}
 }
 
-void Field::MainPhase(Action* Event) {
-	if (Event->GetEntityID() < 3){ /* enemy entity */ 
-		int opcode = 0;
-
-		// -> randomly choose skill of equiment or normal attack
-		int target = 0;
-		while (eventQueue[target = rand() % 3 + 3]->GetObj());
-
-		// -> set data of dice with equiment by its table
-		// -> use skill
-	}
-	else { /* player entity */
-		int opcode = 0;
-		// -> UI returning opCode to choose skill
-		int skillID = 0;
-		
-		// -> UI returning opCode to choose target
-		int targetID = 0;
-		Entity* target;
-		for (int i = 0; i < eventQueue.size(); ++i) {
-			if (targetID == eventQueue[i]->GetEntityID()) {
-				target = eventQueue[i]->GetObj();
-				break;
-			}
-		}
-
-		// -> set data of dice with equiment by its table
-		// -> set dice when use focus
-		// -> use skill
-	}
-}
-
-void Field::BattlePhase(Action* Event) {
-	using namespace std;
-
-	// --------compulate damage stage--------
-	// -> set dice when angry status.
-	Event->GetDice()->RollDice();
-	vector<bool> diceResult = Event->GetDice()->GetResult();
-
-	bool perfect = 1;
-	bool defeat = 1;
-	for (int i = 0; i < diceResult.size(); ++i) {
-		if (diceResult[i]) {
-			defeat = 0;
-		}
-		else {
-			perfect = 0;
-		}
-	}
-
-	if (perfect) {
-		;
-	}
-	if (defeat) {
-		;
-	}
-
-
-
-	// --------compulate atk buff--------
-
-
-
-	// --------compulate def buff--------
-
-
-}
-
-void Field::DamagePhase(Action* Event) {
-	;
-}
-
-void Field::ExitPhase(void) {
-	using namespace std;
-
-	bool AllRoleDead = 1, AllEnemyDead = 1;
-	/*for (int i = 0; i < 3; ++i) {
-		if (!eventQueue[i]->GetObj()) return;
-		if (!(eventQueue[i]->GetObj()->GetStatus() & DEAD)) AllEnemyDead = 0;
-	}
-	for (int i = 3; i < 6; ++i) {
-		if (!eventQueue[i]->GetObj()) return;
-		if (!(eventQueue[i]->GetObj()->GetStatus() & DEAD)) AllRoleDead = 0;
-	}*/
-
-	if (AllRoleDead) {
-		throw exception("Role Dead!\n");
-	}
-	if (AllEnemyDead) {
-		throw exception("Enemy Dead!\n");
-	};
-}
-
 Action* Field::RefreshEvent(void) {
 	using namespace std;
-	/*
+
 	auto cmpPriority = [](Action* x, Action* y) {
 		return x->GetPriority() < y->GetPriority();
 	};
@@ -160,12 +69,272 @@ Action* Field::RefreshEvent(void) {
 	stable_sort(eventQueue.begin(), eventQueue.end(), cmpPA);
 	stable_sort(eventQueue.begin(), eventQueue.end(), cmpSPD);
 	stable_sort(eventQueue.begin(), eventQueue.end(), cmpPriority);
-*/
+
+	// static int a0, a1, a2, a3, a4, a5;	static string a6;
+
 	// update for the next refresh sort
 	eventQueue[0]->SetTurn(eventQueue[0]->GetTurn() + 1);
-	//eventQueue[0]->SetPriority(double(eventQueue[0]->GetTurn() + 1) / eventQueue[0]->GetObj()->GetAttribute().GetSPD());
+	eventQueue[0]->SetPriority(double(eventQueue[0]->GetTurn()) / eventQueue[0]->GetObj()->GetAttribute().GetSPD() * 100);
 
 	return eventQueue[0];
+}
+
+void Field::MainPhase(Action* currEvent) {
+	using namespace std;
+
+	if (currEvent->GetEntityID() < 3) { /* enemy entity */
+		EnemyMainPhase(currEvent);
+	}
+	else { /* player entity */
+		PlayerMainPhase(currEvent);
+	}
+}
+
+void Field::RemoveDeadEntity(void) {
+	for (auto it = eventQueue.begin(); it != eventQueue.end();) {
+		if ((*it)->GetObj()->GetStatus() & DEAD || (*it)->GetObj()->GetStatus() & RETREAT) {
+			it = eventQueue.erase(it);
+		}
+		else {
+			++it;
+		}
+	}
+
+	for (auto it = roles.begin(); it != roles.end();) {
+		if ((*it)->GetStatus() & DEAD || (*it)->GetStatus() & RETREAT) {
+			it = roles.erase(it);
+		}
+		else {
+			++it;
+		}
+	}
+
+	for (auto it = enemys.begin(); it != enemys.end();) {
+		if ((*it)->GetStatus() & DEAD || (*it)->GetStatus() & RETREAT) {
+			it = enemys.erase(it);
+		}
+		else {
+			++it;
+		}
+	}
+}
+
+void Field::PlayerMainPhase(Action* currEvent) {
+CHOICE:
+	UI::BuildVoid(121, 7, 0, 27);
+	UI::PlayerFrame({ 0,1,2,3,4,5 });
+	std::cout << YELLOW;
+	UI::PlayerFrame(currEvent->GetEntityID());
+
+	auto skills = currEvent->GetObj()->GetTotalSkill().GetActive();
+	auto skillToUse = UI::makeChoice(skills, 6, 9);
+	UI::logDivider(currEvent->GetObj()->GetName(), skillToUse.first);
+
+	int diceAmount = skillToUse.first == "Attack" ?
+		currEvent->GetObj()->GetEquipment().GetWeapon().GetDiceAmount() : skills[skillToUse.second].GetDiceAmount();
+	int focus = currEvent->GetObj()->GetAttribute().GetFocus();
+
+	UI::displayDice(diceAmount, 0);
+
+TARGET:
+	auto target = ChooseTarget(currEvent, skills[skillToUse.second].GetTargetType());
+	if (target.empty()) {
+		UI::logEvent("  ʤw    ");
+		UI::logEvent("");
+		goto CHOICE;
+	}
+
+	int focusUse = ChooseFocus(focus, diceAmount);
+	if (focusUse == -1) {
+		UI::logEvent("  ʤw    ");
+		UI::logEvent("");
+		goto TARGET;
+	}
+
+	if (focusUse != 0) {
+		UI::logEvent(" ϥ  " + std::to_string(focusUse) + "  M ` I  ");
+		currEvent->GetObj()->GetAttribute().SetFocus(focus - focusUse);
+		currEvent->GetObj()->GetDice().SetFocusCount(focusUse);
+	}
+	currEvent->GetObj()->useActive(skillToUse.first, target);
+	UI::logEvent("");
+}
+
+std::pair<std::string, int> makeChoice(std::vector<Active> choices) {
+	bool keyState[KeyBoard::INVALID];
+	int select = rand() % choices.size();
+	return { choices[select].GetName(), select };
+}
+
+void Field::EnemyMainPhase(Action* currEvent) {
+	UI::BuildVoid(121, 7, 0, 27);
+	UI::PlayerFrame({ 0,1,2,3,4,5 });
+	std::cout << YELLOW;
+	UI::PlayerFrame(currEvent->GetEntityID());
+
+	auto skills = currEvent->GetObj()->GetTotalSkill().GetActive();
+	int skillNumber;
+	while ((skillNumber = rand() % skills.size()) == 1);
+
+	auto skillToUse = skills[skillNumber];
+	auto target = RandomTarget(currEvent, rand() % skills.size() /* + 3 */);
+	currEvent->GetObj()->useActive(skillToUse.GetName(), target);
+	UI::logEvent("");
+}
+
+std::vector<Entity*> Field::RandomTarget(Action* currEvent, int TargetType) {
+	std::vector<std::string> targetName;
+	std::vector<std::vector<Entity*>> targetPtr;
+	std::vector<Entity*> enemysToEntity;
+	std::vector<Entity*> rolesToEntity;
+	switch (TargetType) {
+	case 0:
+		targetName.push_back(" m Self  n");
+		targetPtr.push_back({ currEvent->GetObj() });
+		break;
+	case 1:
+		for (Enemy* E : enemys) {
+			targetName.push_back(E->GetName());
+			targetPtr.push_back({ E });
+		}
+		break;
+	case 2:
+		targetName.push_back(" m All enemies  n");
+		for (Enemy* E : enemys) {
+			enemysToEntity.push_back(E);
+		}
+		targetPtr.push_back(enemysToEntity);
+		break;
+	case 3:
+		for (Role* R : roles) {
+			targetName.push_back(R->GetName());
+			targetPtr.push_back({ R });
+		}
+		break;
+	case 4:
+		targetName.push_back(" m All teammates  n");
+		for (Role* R : roles) {
+			rolesToEntity.push_back(R);
+		}
+		targetPtr.push_back(rolesToEntity);
+		break;
+	default:
+		for (Enemy* E : enemys) {
+			targetName.push_back(E->GetName());
+			targetPtr.push_back({ E });
+		}
+		for (Role* R : roles) {
+			targetName.push_back(R->GetName());
+			targetPtr.push_back({ R });
+		}
+		break;
+	}
+
+	return targetPtr[TargetType];
+}
+
+std::vector<Entity*> Field::ChooseTarget(Action* currEvent, int TargetType) {
+	std::vector<std::string> targetName;
+	std::vector<std::vector<Entity*>> targetPtr;
+	std::vector<Entity*> enemysToEntity;
+	std::vector<Entity*> rolesToEntity;
+	switch (TargetType) {
+	case 0:
+		targetName.push_back(" m Self  n");
+		targetPtr.push_back({ currEvent->GetObj() });
+		break;
+	case 1:
+		for (Enemy* E : enemys) {
+			targetName.push_back(E->GetName());
+			targetPtr.push_back({ E });
+		}
+		break;
+	case 2:
+		targetName.push_back(" m All enemies  n");
+		for (Enemy* E : enemys) {
+			enemysToEntity.push_back(E);
+		}
+		targetPtr.push_back(enemysToEntity);
+		break;
+	case 3:
+		for (Role* R : roles) {
+			targetName.push_back(R->GetName());
+			targetPtr.push_back({ R });
+		}
+		break;
+	case 4:
+		targetName.push_back(" m All teammates  n");
+		for (Role* R : roles) {
+			rolesToEntity.push_back(R);
+		}
+		targetPtr.push_back(rolesToEntity);
+		break;
+	default:
+		for (Enemy* E : enemys) {
+			targetName.push_back(E->GetName());
+			targetPtr.push_back({ E });
+		}
+		for (Role* R : roles) {
+			targetName.push_back(R->GetName());
+			targetPtr.push_back({ R });
+		}
+		break;
+	}
+	int targetNumber = UI::makeChoice(targetName, 26, 9);
+	if (targetNumber == -1) {
+		return {};
+	}
+	return targetPtr[targetNumber];
+}
+
+int Field::ChooseFocus(int focusPoint, int diceAmount) {
+	int focusUse = 0;
+	bool keyState[KeyBoard::INVALID];
+	UI::displayDice(diceAmount, focusUse);
+	//UI::renewPlayerInfo();
+	while (1) {
+		KeyBoard::keyUpdate(keyState);
+		if (keyState[KeyBoard::EA]) {
+			if (focusUse > 0) {
+				focusUse--;
+			}
+		}
+		else if (keyState[KeyBoard::ED]) {
+			if (focusUse < focusPoint && focusUse < diceAmount) {
+				focusUse++;
+			}
+		}
+		else if (keyState[KeyBoard::ESPACE] || keyState[KeyBoard::EENTER]) {
+			return focusUse;
+		}
+		else if (keyState[KeyBoard::EESC]) {
+			return -1;
+		}
+		else {
+			continue;
+		}
+		UI::displayDice(diceAmount, focusUse);
+		//UI::renewPlayerInfo();
+	}
+}
+
+void Field::ExitPhase(void) {
+	using namespace std;
+
+	bool AllRoleDead = 1, AllEnemyDead = 1;
+	for (auto it : enemys) {
+		if (!(it->GetStatus() & DEAD)) AllEnemyDead = 0;
+	}
+	for (auto it : roles) {
+		if (!(it->GetStatus() & DEAD)) AllRoleDead = 0;
+	}
+
+	if (AllRoleDead) {
+		throw exception("Role Dead!\n");
+	}
+	if (AllEnemyDead) {
+		throw exception("Enemy Dead!\n");
+	};
 }
 
 
@@ -173,10 +342,13 @@ Action* Field::RefreshEvent(void) {
 // constructor
 Action::Action(Entity* val, uint8_t mode, uint8_t ID)
 	:obj(val), turn(0), entityID(ID), count(new StatusCount{ 0, 0, 0, 0 }), dice(nullptr) {
-	priority = 0;// 1.0 / val->GetAttribute().GetSPD() * 100;
+	priority = 1.0 / val->GetAttribute().GetSPD() * 100;
 }
 
-Field::Field(std::vector<Entity*> players, std::vector<Entity*> enemies) : currEvent(nullptr) {
+Field::Field(std::vector<Role*> players, std::vector<Enemy*> enemies) : currEvent(nullptr) {
+	roles = players;
+	enemys = enemies;
+
 	for (int i = 0; i < players.size(); ++i) {
 		eventQueue.push_back(new Action(players[i], ROLE, i + 3));
 	}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-﻿#include "Control/WorldMap.h"
+﻿#include "Includes/Entity/Role.h"
+#include "Includes/Entity/Enemy.h"
+#include "Includes/Gadget/Equipment.h"
+#include "Includes/Gadget/EquipmentTable.h"
+#include "Includes/Gadget/SkillTable.h"
+#include "Includes/Display/UI.h"
 #include <Process.h>
+
 #include <string>
 
 int main() {
@@ -31,93 +37,97 @@ int main() {
     //system("Pause");
 
 
-    Process::HandlePreBattle({ &dragon , &porfDai  , &tonya }, { &hero ,&profPao  , &myWife });
+    //Process::HandlePreBattle({ &dragon , &porfDai  , &tonya }, { &hero ,&profPao  , &myWife });
 
     //UI::PreBattle({ &dragon , &porfDai  , &tonya }, { &hero ,&profPao  , &myWife });
     UI::moveCursor(2, 9);
 
+    Field battle({ &hero ,&profPao  , &myWife }, { &dragon , &porfDai  , &tonya });
+    battle.StartBattle();
 
-    std::vector<Entity*> targets;
-    targets.push_back(&dragon);
-    for (int i = 0; i < 5; i++) {
-        auto skills = hero.GetTotalSkill().GetActive();
-        std::string skillToUse = UI::makeChoice(skills, 6, 9).first;
-        UI::logDivider(skillToUse);
-        hero.useActive(skillToUse, { &dragon });
-    }
+    /*
+        std::vector<Entity*> targets;
+        targets.push_back(&dragon);
+        for (int i = 0; i < 2; i++) {
+            auto skills = hero.GetTotalSkill().GetActive();
+            std::string skillToUse = UI::makeChoice(skills, 6, 9).first;
+            UI::logDivider(skillToUse);
+            hero.useSkill(skillToUse, { &dragon });
+        }
 
-    UI::logDivider("");
-    hero.GetDice().SetFocusCount(3);
-    hero.useActive("Attack", { &dragon });
-    UI::logDivider("");
-    hero.useActive("Flee", { &hero });
-    UI::logDivider("");
-    hero.useActive("ShockBlast", { &dragon });
-    UI::logDivider("");
-    hero.useActive("Heal", { &hero });
-    UI::logDivider("");
-    hero.useActive("Heal", { &dragon });
-    UI::logDivider("");
+        UI::logDivider("");
+        hero.GetDice().SetFocusCount(3);
+        hero.useSkill("Attack", { &dragon });
+        UI::logDivider("");
+        hero.useSkill("Flee", { &hero });
+        UI::logDivider("");
+        hero.useSkill("ShockBlast", { &dragon });
+        UI::logDivider("");
+        hero.useSkill("Heal", { &hero });
+        UI::logDivider("");
+        hero.useSkill("Heal", { &dragon });
+        UI::logDivider("");
 
-    UI::logDivider("Equip WoodenSword");
-    hero.equip("WoodenSword");
-    hero.useActive("SpeedUp", { &hero });
+        UI::logDivider("Equip WoodenSword");
+        hero.equip("WoodenSword");
+        hero.useSkill("SpeedUp", { &hero });
 
-    UI::logDivider("Hero");
-    hero.GetTotalAttribute();
-    UI::logDivider("Dragon");
-    dragon.GetTotalAttribute();
+        UI::logDivider("Hero");
+        hero.GetTotalAttribute();
+        UI::logDivider("Dragon");
+        dragon.GetTotalAttribute();
 
 
-    UI::logDivider("Equip Equipment");
-    hero.equip("GiantHammer");
-    hero.equip("LaurelWreath");
-    hero.equip("HolyGrail");
+        UI::logDivider("Equip Equipment");
+        hero.equip("GiantHammer");
+        hero.equip("LaurelWreath");
+        hero.equip("HolyGrail");
 
-    //hero.GetTotalAttribute().display();
-    //std::cout << std::endl;
+        //hero.GetTotalAttribute().display();
+        //std::cout << std::endl;
 
-    UI::logDivider("Apply Skill");
-    //hero.GetTotalSkill().display();
+        UI::logDivider("Apply Skill");
+        //hero.GetTotalSkill().display();
 
-    //重複定義
-    //std::vector<Entity*> targets;
-    //targets.push_back(&dragon);
+        //重複定義
+        //std::vector<Entity*> targets;
+        //targets.push_back(&dragon);
 
-    UI::logDivider("1");
-    hero.useActive("Attack", targets);
-    UI::logDivider("2");
-    hero.useActive("Attack", targets);
-    UI::logDivider("3");
-    hero.useActive("Attack", targets);
-    UI::logDivider("4");
-    hero.useActive("Attack", targets);
-    UI::logDivider("5");
-    hero.useActive("Attack", targets);
-    UI::logDivider("6");
-    hero.useActive("Attack", targets);
-    UI::logDivider("7");
-    hero.useActive("Attack", targets);
-    UI::logDivider("8");
-    hero.useActive("Flee", targets);
-    UI::logDivider("9");
-    hero.useActive("Heal", targets);
-    UI::logDivider("10");
-    //hero.useActive("SpeedUp", targets);
+        UI::logDivider("1");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("2");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("3");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("4");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("5");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("6");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("7");
+        hero.useSkill("Attack", targets);
+        UI::logDivider("8");
+        hero.useSkill("Flee", targets);
+        UI::logDivider("9");
+        hero.useSkill("Heal", targets);
+        UI::logDivider("10");
+        //hero.useSkill("SpeedUp", targets);
 
-    UI::logDivider("UnEquip Equipment");
-    hero.unEquip(hero.GetEquipment().GetWeapon().GetName());
-    hero.unEquip(hero.GetEquipment().GetArmor().GetName());
-    hero.unEquip(hero.GetEquipment().GetAccessory().GetName());
-    hero.useActive("Attack", targets);
-    hero.useActive("Flee", targets);
-    //hero.useActive("Heal", targets);
-    //hero.useActive("SpeedUp", targets);
-    UI::logDivider("Hero");
-    //hero.GetTotalAttribute().display();
-    UI::logDivider("Dragon");
-    //dragon.GetTotalAttribute().display();
-    //std::cout << std::endl;
+        UI::logDivider("UnEquip Equipment");
+        hero.unEquip(hero.GetEquipment().GetWeapon().GetName());
+        hero.unEquip(hero.GetEquipment().GetArmor().GetName());
+        hero.unEquip(hero.GetEquipment().GetAccessory().GetName());
+        hero.useSkill("Attack", targets);
+        hero.useSkill("Flee", targets);
+        //hero.useSkill("Heal", targets);
+        //hero.useSkill("SpeedUp", targets);
+        UI::logDivider("Hero");
+        //hero.GetTotalAttribute().display();
+        UI::logDivider("Dragon");
+        //dragon.GetTotalAttribute().display();
+        //std::cout << std::endl;
+        */
 
     return 0;
 }
