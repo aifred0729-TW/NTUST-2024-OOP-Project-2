6d2df82138a2b50b37c98637b3f30d0858d5aecc
AkitaCodingPeasant <162419584+AkitaCodingPeasant@users.noreply.github.com>
2024-06-09 22:50:01 +0800

    REFACTOR: 把WorldMap class 改成 static

--- Includes/Control/WorldMap.h
+++ Includes/Control/WorldMap.h
@@ -16,19 +16,16 @@
 class WorldMap {
 private:
     // Map Structure
-    // 0 = Player
+    // 0 = Player (不他不該出現在這裡)
     // 1 = Ground (Allow to Pass)
     // 2 = Wall   (Not Allow to Pass)
     // 3 = Tree   (Not Allow to Pass)
     // 4 = Water  (Not Allow to Pass)
-    // 5 = Shop   (Not Allow to Pass)
-    // 6 = Enemy  (Not Allow to Pass)
+    // 5 = Shop   (Allow to Pass) (修改至可互動物件)
+    // 6 = Enemy  (Allow to Pass) (修改至可互動物件)
 
-    std::vector<std::vector<int>>  map; // Map Storge
-    std::vector<std::vector<bool>> fog; // War Fog (Make some lamp?)
-
-    void loadMap(std::string mapFile);
-    void loadFog();
+    static std::vector<std::vector<int>>  map; // Map Storge
+    static std::vector<std::vector<bool>> fog; // War Fog (Make some lamp?)
 
 public:
     WorldMap() { ; };
@@ -38,11 +35,17 @@ public:
         loadFog();
     }
 
-    std::vector<std::vector<int>> GetMap(void);
-    void SetMap(int row, int col, MAP_ELEMENT element);
+    //只能使用一次
+    static void loadMap(std::string mapFile);
+    //只能使用一次
+    static void loadFog();
 
-    std::vector<std::vector<bool>> GetFog(void);
-    void SetFog(int row, int col);
+    static std::vector<std::vector<int>> GetMap(void);
+
+    static void SetMap(int row, int col, MAP_ELEMENT element);
+
+    static std::vector<std::vector<bool>> GetFog(void);
+    static void SetFog(int row, int col);
 
 public:
     // ...
