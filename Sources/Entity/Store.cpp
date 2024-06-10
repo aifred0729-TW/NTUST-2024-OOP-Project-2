95b32e0a32e5d61e4dd8a054f28bc22c70b8419a
AkitaCodingPeasant <162419584+AkitaCodingPeasant@users.noreply.github.com>
2024-06-11 05:33:12 +0800

    FEAT: 完善商店系統

--- Sources/Entity/Store.cpp
+++ Sources/Entity/Store.cpp
@@ -2,6 +2,7 @@
 #include "ItemTable.h"
 #include "Displayer.h"
 #include "Backpack.h"
+#include "UI.h"
 
 // Public
 Store::Store(std::string name, std::pair<int, int> position) {
@@ -25,7 +26,6 @@ std::string Store::GetName(void) const {
 void Store::SellItemTo(Role* player) {
     std::vector<std::string> ITEM_TABLE = { "Godsbeard", "GoldenRoot", "TeleportScroll", "Tent", "WoodenSword", "Hammer", "GiantHammer", "MagicWand",
     "RitualSword", "WoodenShield", "PlateArmor", "LeatherArmor", "Robe", "LaurelWreath", "HolyGrail", "Shoes", "Bracelet" };
-    using namespace Displayer;
     ItemTable itemList;
     uint16_t price, money;
     int index = 0;
@@ -38,7 +38,7 @@ void Store::SellItemTo(Role* player) {
             }
         }
     }
-    int choiceIndex = makeChoice(ITEM_TABLE, 100, 10); // 要跟左呈討論makeChoice
+    int choiceIndex = UI::makeChoice(ITEM_TABLE, 100, 10); // 要跟左呈討論makeChoice
     ITEM_TABLE = { "Godsbeard", "GoldenRoot", "TeleportScroll", "Tent", "WoodenSword", "Hammer", "GiantHammer", "MagicWand",
     "RitualSword", "WoodenShield", "PlateArmor", "LeatherArmor", "Robe", "LaurelWreath", "HolyGrail", "Shoes", "Bracelet" };
     for (const auto& pair : itemList.itemMap) {
@@ -60,6 +60,49 @@ void Store::SellItemTo(Role* player) {
             }
             player->SetMoney(money - price);
             player->backpack.addItem(pair.second);
+            return;
+        }
+    }
+}
+
+void Store::SellItemTo() {
+    std::vector<std::string> ITEM_TABLE = { "Godsbeard", "GoldenRoot", "TeleportScroll", "Tent", "WoodenSword", "Hammer", "GiantHammer", "MagicWand",
+    "RitualSword", "WoodenShield", "PlateArmor", "LeatherArmor", "Robe", "LaurelWreath", "HolyGrail", "Shoes", "Bracelet" };
+    ItemTable itemList;
+    uint16_t price, money;
+    int index = 0;
+    for (const auto& pair : itemList.itemMap) {
+        for (int i = 0; i < ITEM_TABLE.size(); ++i) {
+            if (pair.first == ITEM_TABLE[i]) {
+                ITEM_TABLE[i].resize(45, ' ');
+                ITEM_TABLE[i] += "$ ";
+                ITEM_TABLE[i] += std::to_string(pair.second->getPrice());
+                break;
+            }
+        }
+    }
+    int choiceIndex = UI::makeChoice(ITEM_TABLE, 126, 2); // 要跟左呈討論makeChoice
+    ITEM_TABLE = { "Godsbeard", "GoldenRoot", "TeleportScroll", "Tent", "WoodenSword", "Hammer", "GiantHammer", "MagicWand",
+    "RitualSword", "WoodenShield", "PlateArmor", "LeatherArmor", "Robe", "LaurelWreath", "HolyGrail", "Shoes", "Bracelet" };
+    for (const auto& pair : itemList.itemMap) {
+        if (pair.first == ITEM_TABLE[choiceIndex]) {
+            if (!pair.second->isStackable()) { // 由於裝備不能疊加，所以多判斷背包是否已經有裝備
+                std::vector<Item*> items = Role::backpack.getItems();
+                for (int i = 0; i < items.size(); ++i) {
+                    if (items[i] == pair.second) {
+                        std::cout << "you have bought this equipment.\n"; // 要跟左呈討論display
+                        return;
+                    }
+                }
+            }
+            price = pair.second->getPrice();
+            money = Role::GetMoney();
+            if (price > money) {
+                std::cout << "you can't buy.\n"; // 要跟左呈討論display
+                return;
+            }
+            Role::SetMoney(money - price);
+            Role::backpack.addItem(pair.second);
             return;
         }
     }
