b5deef9ac95d524a5d7a08794cd284518c3510d4
AkitaCodingPeasant <162419584+AkitaCodingPeasant@users.noreply.github.com>
2024-06-11 05:56:06 +0800

    FEAT: 套用Process到Game

--- Sources/Control/Game.cpp
+++ Sources/Control/Game.cpp
@@ -5,6 +5,7 @@
 #include <KeyBoard.h>
 #include <EquipmentTable.h>
 #include "Dice.h"
+#include "Color.h"
 
 // Private
 
@@ -63,6 +64,11 @@ void Game::Initialize() {
     static Enemy boomer_tonya("胖子炸彈 - Tonya", 5, 3);
     enemys = { &fat_tonya ,&troll_tonya ,&boomer_tonya };
 
+    WorldMap::SetRoles({ &Kazusa ,&Shiroko ,&Hoshino });
+    WorldMap::SetEnemys({ &fat_tonya ,&troll_tonya ,&boomer_tonya });
+
+    WorldMap::loadMap("W-1.txt");
+
     return;
 }
 
@@ -79,8 +85,6 @@ void Game::MainProcess(void) {
     system("Pause");
     system("CLS");
 
-
-    WorldMap::loadMap("W-1.txt");
     UI::PreWorldMap(roles);
     // vector<Role*> executionRoles;
 
@@ -98,30 +102,112 @@ void Game::MainProcess(void) {
 
 int Game::OnePlayerMovePhase(Role* currentActRole) {
     // Move Stage
+    //投骰子
     int movementPoint = GenerateMovementPoint(currentActRole);
-
+    std::cout << BG_WHITE;
+    UI::displayMapGrid();
+    UI::PrintWorldMap();
     bool keyState[KeyBoard::INVALID];
-    KeyBoard::keyUpdate(keyState);
-    if (keyState[KeyBoard::EP]) {
-    }
-    else if (keyState[KeyBoard::EI]) {
-        // backpack process
+    static bool distanceDisplayWork = 0;
+
+    std::cout << BG_WHITE;
+    UI::displayMapGrid();
+
+    while (1) {
+
+        KeyBoard::keyUpdate(keyState);
+        if (keyState[KeyBoard::EP]) {
+        }
+        else if (keyState[KeyBoard::EI]) {
+            // backpack process
+
+        }
+        else if (keyState[KeyBoard::EDU] || keyState[KeyBoard::EDL] || keyState[KeyBoard::EDR] || keyState[KeyBoard::EDD]) {
+            // - Roll Dice
+            // Display Please Roll Dice (UI)
+            // displayRollDice();
+            // ditect entity is interactiveable
+        }
+        else if (keyState[KeyBoard::EW]) {
+            WorldMap::movePos(0, -1);
+        }
+        else if (keyState[KeyBoard::EA]) {
+            WorldMap::movePos(-1, 0);
+        }
+        else if (keyState[KeyBoard::ES]) {
+            WorldMap::movePos(0, 1);
+        }
+        else if (keyState[KeyBoard::ED]) {
+            WorldMap::movePos(1, 0);
+        }
+        else if (keyState[KeyBoard::ESPACE] || keyState[KeyBoard::EENTER]) {
+        }
+        else if (keyState[KeyBoard::EESC]) {
+        }
+        else {
+            continue;
+        }
+
+        //if (distanceDisplayWork == 1) {
+        std::cout << BG_WHITE;
+        UI::displayMapGrid();
+        distanceDisplayWork = 0;
+        //}
+
+        UI::PrintWorldMap();
+        if (!WorldMap::GetRect().stores.empty()) {
+            UI::BuildFrame(121, 0, 179, 28);
+
+            std::vector<Store*> storesToDisplay;
+            //UI::displayDiceMove({ 0, 1, 0, 1, 0, 1 });
+            //WorldMap::GetRect().stores[0]->SellItemTo();
+        }
+        else if (!WorldMap::GetRect().enemys.empty() || !WorldMap::GetRect().roles.empty()) {
+
+            UI::BuildFrame(121, 0, 179, 28);
+
+            std::vector<Entity*> entitysToDisplay;
+            if (!WorldMap::GetRect().enemys.empty()) {
+                distanceDisplayWork = 1;
+                std::cout << BG_WHITE;
+                UI::displayMapGrid();
+                std::cout << BG_BRIGHT_RED;
+                UI::distanceDisplay(0, 0, 3);
+                for (auto E : WorldMap::GetRect().enemys) {
+                    entitysToDisplay.push_back(E);
+                }
+            }
+            else if (!WorldMap::GetRect().roles.empty()) {
+                distanceDisplayWork = 1;
+                std::cout << BG_WHITE;
+                UI::displayMapGrid();
+                std::cout << BG_BRIGHT_CYAN;
+                UI::distanceDisplay(0, 0, 3);
+                for (auto R : WorldMap::GetRect().roles) {
+                    entitysToDisplay.push_back(R);
+                }
+            }
+            UI::displayPlayerInfo(121, 0, entitysToDisplay);
+        }
+        else {
+            std::cout << BG_BRIGHT_YELLOW;
+            int x = currentActRole->GetPosition().first - WorldMap::getPos().first;
+            int y = currentActRole->GetPosition().second - WorldMap::getPos().second;
+            UI::distanceDisplay(x, y, movementPoint);
+
+            std::cout << BG_BRIGHT_CYAN;
+            UI::distanceDisplay(0, 0, 0);
+        }
+        UI::moveCursor(0, 0);
+        //UI::renewPlayerInfo();
+        // check is on interactiveable object
+        // if checkIsOnShop
+        // enterShop
+
+        // if checkIsOnEnemy
+        // enterCombat
 
     }
-    else if (keyState[KeyBoard::EDU] || keyState[KeyBoard::EDL] || keyState[KeyBoard::EDR] || keyState[KeyBoard::EDD]) {
-        // - Roll Dice
-        // Display Please Roll Dice (UI)
-        // displayRollDice();
-        // ditect entity is interactiveable
-
-
-    }
-    // check is on interactiveable object
-    // if checkIsOnShop
-    // enterShop
-
-    // if checkIsOnEnemy
-    // enterCombat
 
     return 0;
 }
