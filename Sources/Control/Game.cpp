6afb0b47ff2fa1a4a44708fa8769eba97b1ecfb4
AkitaCodingPeasant <162419584+AkitaCodingPeasant@users.noreply.github.com>
2024-06-11 02:12:12 +0800

    FIX: 修改Entity 起始座標宣告

--- Sources/Control/Game.cpp
+++ Sources/Control/Game.cpp
@@ -8,81 +8,81 @@
 // Private
 
 int Game::Move(void) {
-	return 0;
+    return 0;
 }
 
 int Game::GetKeyboard(void) {
-	return 0;
+    return 0;
 }
 
 int Game::GiveRandomAttribate(void) {
-	return 0;
+    return 0;
 }
 
 // Public
 
 void Game::Initialize() {
-	SkillTable::Initialize();
-	EquipmentTable::Initialize();
-	ItemTable::Initialize();
-	Role Kazusa("杏山千紗");
-	Role Shiorko("砂狼白子");
-	Role Hoshino("小鳥游星野");
+    SkillTable::Initialize();
+    EquipmentTable::Initialize();
+    ItemTable::Initialize();
+    Role Kazusa("杏山千紗", 1, 1);
+    Role Shiorko("砂狼白子", 3, 1);
+    Role Hoshino("小鳥游星野", 5, 1);
 
-	Enemy fat_tonya("胖子 - Tonya");
-	Enemy troll_tonya("巨魔 - Tonya");
-	Enemy boomer_tonya("胖子炸彈 - Tonya");
+    Enemy fat_tonya("胖子 - Tonya", 1, 3);
+    Enemy troll_tonya("巨魔 - Tonya", 3, 3);
+    Enemy boomer_tonya("胖子炸彈 - Tonya", 5, 3);
 
 
 
-	return;
+    return;
 }
 
 void Game::MainProcess(void) {
-	using namespace std;
+    using namespace std;
 
-	Initialize();
-	cout << "done" << endl;
-	UI::checkConsoleSize(50, 180);
-	UI::ShowMenu();
+    Initialize();
+    cout << "done" << endl;
+    UI::checkConsoleSize(50, 180);
+    UI::ShowMenu();
 
-	system("Pause");
-	system("CLS");
+    system("Pause");
+    system("CLS");
 
-	bool keyState[KeyBoard::INVALID];
+    bool keyState[KeyBoard::INVALID];
 
-	while (true) {
-		// Move Stage
-		KeyBoard::keyUpdate(keyState);
-		if (keyState[KeyBoard::EP]) {
-		}
-		else if (keyState[KeyBoard::EI]) {
-			// backpack process
-			; 
-		}
-		else if (keyState[KeyBoard::EDU] || keyState[KeyBoard::EDL] || keyState[KeyBoard::EDR] || keyState[KeyBoard::EDD]) {
-			// - Roll Dice
-			// Display Please Roll Dice (UI)
-			// displayRollDice();
-			// ditect entity is interactiveable
+    while (true) {
+        // Move Stage
+        KeyBoard::keyUpdate(keyState);
+        if (keyState[KeyBoard::EP]) {
+        }
+        else if (keyState[KeyBoard::EI]) {
+            // backpack process
+            ;
+        }
+        else if (keyState[KeyBoard::EDU] || keyState[KeyBoard::EDL] || keyState[KeyBoard::EDR] || keyState[KeyBoard::EDD]) {
+            // - Roll Dice
+            // Display Please Roll Dice (UI)
+            // displayRollDice();
+            // ditect entity is interactiveable
 
-			;
-		}
-		// check is on interactiveable object
-		// if checkIsOnShop
-		// enterShop
-		
-		// if checkIsOnEnemy
-		// enterCombat
-	}
+            ;
+        }
+        // check is on interactiveable object
+        // if checkIsOnShop
+        // enterShop
 
-	return;
+        // if checkIsOnEnemy
+        // enterCombat
+    }
+
+    return;
 }
 
 bool Game::checkIsOnShop() {
-	using namespace std;
+    using namespace std;
 
-	// if 
+    // if 
 
-	return 0;
+    return 0;
 }
